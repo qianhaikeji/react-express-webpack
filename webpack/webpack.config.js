ab6c395a1515b2eda72d8af72b133c86855136bb
deng han <miracledenghan@163.com>
2016-05-18 14:30:51 +0800

    modify webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,9 @@
 var webpack = require('webpack')
 var path = require("path")
+var ExtractTextPlugin = require("extract-text-webpack-plugin")
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+var extractCSS = new ExtractTextPlugin('[hash:8].style.css', { allChunks: true });
 
 module.exports = {
   entry: process.env.NODE_ENV === 'production' ? {
@@ -20,6 +23,7 @@ module.exports = {
     new webpack.optimize.UglifyJsPlugin({
           compress: { warnings: false }
     }),
+    extractCSS,
     new HtmlWebpackPlugin({
       title: "react-express-webpack",
       template: path.join(__dirname,'../client/index.prod.html'),
@@ -30,10 +34,13 @@ module.exports = {
         collapseWhitespace:false    //删除空白符与换行符
       }
     })
-  ] : [],
+  ] : [
+    extractCSS
+  ],
 
   module: {
     loaders: [
+      { test: /\.css$/, exclude: /node_modules/, loader: extractCSS.extract('style-loader', 'css-loader') },
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
     ]
   }
